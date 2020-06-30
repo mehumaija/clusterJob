64861d49a5b1186c37fec3b0844742e250a3d16a
mehumaija <57179814+mehumaija@users.noreply.github.com>
2020-06-30 13:53:21 +0300

    Deleted useless methods, abstracted

--- src/main/java/edu/ucsf/rbvi/clusterJob/internal/io/RemoteServer.java
+++ src/main/java/edu/ucsf/rbvi/clusterJob/internal/io/RemoteServer.java
@@ -3,6 +3,7 @@ package edu.ucsf.rbvi.clusterJob.internal.io;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 
 import java.util.ArrayList;
@@ -21,6 +22,8 @@ import org.apache.http.entity.StringEntity;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.apache.log4j.Logger;
 import org.cytoscape.jobs.CyJobData;
 import org.cytoscape.jobs.CyJobDataService;
 import org.cytoscape.jobs.CyJobExecutionService;
@@ -43,19 +46,14 @@ public class RemoteServer {
 	private String LOCAL_PATH = "http://localhost:8000/";
 	private String PROD_PATH = "http://webservices.rbvi.ucsf.edu/rest/api/v1/";
 	private String inputFile = "C:\\Users\\maikk\\Documents\\gsoc\\test_input.csv";
-	private CyJobExecutionService executionService;
-	private CyJobDataService dataService;
-	private ClusterJobHandler jobHandler;
-	
-	public RemoteServer(CyJobExecutionService executionService, CyJobDataService dataService, ClusterJobHandler jobHandler) {
-		this.executionService = executionService;
-		this.dataService = dataService;
-		this.jobHandler = jobHandler;
-	}
 	
 	public RemoteServer() {
 	}
 		
+	public String getBasePath() {
+		return PROD_PATH;
+	}
+	
 		//make the choice between different clustering algorithms/servers, this works
 	public String getServiceURI(String service) {
 		
@@ -127,65 +125,11 @@ public class RemoteServer {
 		return edgesList;
 		
 	}
-	
-
-	
-	//read the file and create JSONObject, this works
-	public JSONObject createJSONObjectFromData() throws ParseException {
-
-		System.out.println("Converting to JSON: " + this.inputFile);
-			
-		Map<String, JSONArray> data = new HashMap<>();
-		JSONArray nodesArray = new JSONArray();
-		List<String> nodesList = new ArrayList<>();
-		List<List<String>> edgesList = new ArrayList<>();
-
-		try (Scanner scanner = new Scanner(new File(this.inputFile))) {
-
-			while (scanner.hasNextLine()) {
-				String edge = scanner.nextLine();
-				
-				String [] nodesAndWeights = edge.split(",");
-				ArrayList<String> edgeArray = new ArrayList<>();
-				for (int i = 0; i < 3; i++) {
-					edgeArray.add(nodesAndWeights[i]);
-				}
-				edgesList.add(edgeArray);
-				
-				for (int i = 0; i < 2; i++) {
-					if (!nodesArray.contains(nodesAndWeights[i])) {
-						nodesArray.add(nodesAndWeights[i]);
-					}
-				}
-			}
-			
-		} catch (Exception e) {
-			System.out.println("Exception reading the file containing the edges data:" + e.getMessage());
-		}
-		
-		JSONArray JSONedgesList  = new JSONArray();
-		for (List<String> edgeArray : edgesList) {
-			JSONedgesList.add(edgeArray);
-		}
-		
-		data.put("nodes", nodesArray);
-		data.put("edges", JSONedgesList);
-		
-		
-	    JSONObject jsonData = new JSONObject();
-	    for (String key : data.keySet()) {
-	        Object value = data.get(key);
-	        jsonData.put(key, value);
-	    }
-	    return jsonData;
-
-	}
 
 		//sends the data
 		//you should use a JSONParser to parse the reply and return that.
 		//the URI is the whole URI of the service
 		//returns the status code and JSONObject (JobID)
-		//works! returns JSON jobID: {"job_id":"136b62d2-5904-413c-a430-b041a23a69bd"}
 	public JSONObject postFile(String uri, JSONObject jsonData) throws Exception {
 		System.out.println("Posting on: " + uri);
 		CloseableHttpClient httpClient = HttpClients.createDefault();  //client = browser --> executes in the default browser of my computer?
@@ -230,7 +174,9 @@ public class RemoteServer {
 		System.out.println("Fetching JSON from: " + uri);
 		
 		CloseableHttpClient httpclient = HttpClients.createDefault();  //client = browser --> executes in the default browser of my computer?
-		CloseableHttpResponse response = getGETresponse(uri, httpclient);
+		HttpGet httpGet = new HttpGet(uri);
+		System.out.println("HttpGET: " + httpGet.toString());
+		CloseableHttpResponse response = httpclient.execute(httpGet);
 		
 		System.out.println("HttpGET response: " + response.toString());
 		
@@ -240,102 +186,29 @@ public class RemoteServer {
 			return null;
 		}
 		HttpEntity entity = response.getEntity();
+		System.out.println("HttpEntity: " + response.getEntity());
 		BufferedReader reader = new BufferedReader(new InputStreamReader(entity.getContent()));
 		
-		JSONObject json = (JSONObject) new JSONParser().parse(reader);
+		System.out.print("Status from HttpGET response: ");
+		JSONObject json= new JSONObject();
+		String line = "";
+		Object message = null;
+		while ((line = reader.readLine()) != null) {
+			System.out.println(line);
+			json.put("jobStatus", line);
+			if (json.containsKey("message")) {
+				message = json.get("message");
+				json.put("message: ", message);
+			} else {
+				json.put("message", line);
+			}
+		}
+		
+		System.out.println("JSON status: " + json);
+	   
 		return json; //= dictionary, take it and poll from this the status key Map<Key, value> and for key status there is some answer, JSON similar to xml but easier
 	}
-	
-	private CloseableHttpResponse getGETresponse(String uri, CloseableHttpClient httpclient) throws Exception {
-		HttpGet httpGet = new HttpGet(uri);
-		System.out.println("HttpGET: " + httpGet.toString());
-		CloseableHttpResponse response = httpclient.execute(httpGet);
-		
-		return response; //pull the status and --> text string and map to the ENUM --> return ENUM
-	}
-	
-	//is this the correct way of getting the key: status? does this give the information we need. My idea here is that the method gets
-	//the JSONObject from fetchJSON and gets the key "status" that has the value of status and returns it .toString()
-	public String getJobStatusString(JSONObject JSONresponse) {
-		Object status = JSONresponse.get("status");
-		return status.toString();
-	}
-	
-	//this method maps the status String returned by getJobStatusString with CyJobStatus.Status and returns the equivalent status
-	//how can I find out what words the JSONObject status uses?
-	public CyJobStatus.Status mapStringStatusToCyJobStatus(String statusString) {
 
-		if (statusString.equals("done")) {
-			return Status.FINISHED;
-		} else if (statusString.equals("fail")) {
-			return Status.ERROR;
-		}
-		
-		return Status.UNKNOWN;
-	}
-	
-	private CyJobStatus getStatus(JSONObject obj) {
-		if (obj.containsKey("jobStatus")) {
-			Status status = Status.valueOf((String)obj.get("jobStatus"));
-			String message = "";
-			if (obj.containsKey("message")) {
-					message = (String)obj.get("message");
-				return new CyJobStatus(status, message);
-			}
-			return new CyJobStatus(status, message);
-		} else if (obj.containsKey("errorMessage")) {
-			return new CyJobStatus(Status.ERROR, (String)obj.get("errorMessage"));
-		}
-		return null;
-	}
-		
-	public void submitJob() {
-		
-			//first, make JSONObject of the data file
-		JSONObject data = null;
-		try {
-			System.out.println("Creating JSON");
-			data = createJSONObjectFromData();
-			System.out.println("JSON created from data");
-		} catch (Exception e) {
-			System.out.println("Error in creating JSONObject from data: " + e.getMessage());
-		}
-		System.out.println("JSON data: " + data);
-		
-			//choose the service, now leiden
-		String serviceURI = getServiceURI("leiden");
-			
-			//POST (send) the data to the server and get the jobID as the result
-		JSONObject jobIDResponse = null;
-		try {
-			jobIDResponse = postFile(serviceURI, data);
-		} catch (Exception e) {
-			System.out.println("Exception in postFile: " + e.getMessage());
-		}
-		
-		System.out.println("JSON jobID: " + jobIDResponse.toString());
-		
-			//gets the jobID from the JSON. Is this getter necessary? Can I just toString() the whole JSON response?
-		String jobID = jobIDResponse.get("job_id").toString();
-		System.out.println("Job ID: " + jobID);
-		
-		//ClusterJob clusterJob = new ClusterJob("ClusterJob", PROD_PATH, executionService, dataService, jobHandler, jobID);
-		
-			//GET the response containing the status from the server
-		JSONObject statusResponse = null;
-		try {
-			statusResponse = fetchJSON(serviceURI);
-		} catch (Exception e) { 
-			System.out.println("Exception in fetchJSON: " + e.getMessage());
-		}
-		
-		CyJobStatus jobStatus = getStatus(statusResponse);
-		System.out.println(jobStatus);
-
-	}
-	
-	//need to return the jobID and the status, only that is important for now!!
-		
 
 
 }
