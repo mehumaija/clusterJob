40aa895c0af6f26a95747c9213ed3ae5688c5116
mehumaija <57179814+mehumaija@users.noreply.github.com>
2020-06-30 13:55:43 +0300

    Deleted unused imports

--- src/main/java/edu/ucsf/rbvi/clusterJob/internal/io/RemoteServer.java
+++ src/main/java/edu/ucsf/rbvi/clusterJob/internal/io/RemoteServer.java
@@ -2,43 +2,22 @@ package edu.ucsf.rbvi.clusterJob.internal.io;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
 
 import org.apache.http.HttpEntity;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.entity.StringEntity;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
-import org.apache.log4j.Logger;
-import org.cytoscape.jobs.CyJobData;
-import org.cytoscape.jobs.CyJobDataService;
-import org.cytoscape.jobs.CyJobExecutionService;
-import org.cytoscape.jobs.CyJobStatus;
-import org.cytoscape.jobs.CyJobStatus.Status;
-import org.cytoscape.work.TaskMonitor;
-import org.json.simple.JSONArray;
+
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-
-import edu.ucsf.rbvi.clusterJob.internal.handlers.ClusterJobHandler;
-import edu.ucsf.rbvi.clusterJob.internal.model.ClusterJob;
-import edu.ucsf.rbvi.clusterJob.internal.model.ClusterJobData;
-import edu.ucsf.rbvi.clusterJob.internal.model.ClusterJobDataService;
 
 
 public class RemoteServer {
